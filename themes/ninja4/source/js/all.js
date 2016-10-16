74a6c2df42632836e736364ee500cc484ef3500f
Andrey <ghostmancer@gmail.com>
2016-10-15 21:05:57 -0700

    Finished front page

--- themes/ninja4/source/js/all.js
+++ themes/ninja4/source/js/all.js
@@ -1,7 +1,3 @@
-document.querySelector('.experience-more').addEventListener("click", function(e) {
-    document.querySelector('.section-experience .portfolio-section-content').classList.add('expanded');
-});
-
 var elements = document.querySelectorAll('.experience-toggle')
 for(var i = 0; i < elements.length; i++) {
     elements[i].addEventListener("click", function(e) {
@@ -9,5 +5,3 @@ for(var i = 0; i < elements.length; i++) {
         e.target.parentElement.parentElement.classList.toggle('expanded');
     });
 }
-
-visualizer.init();
