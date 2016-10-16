32dcc0f3e919f2d3764fe4388f5b26b09f47da0b
Andrey <ghostmancer@gmail.com>
2016-10-16 13:03:11 -0700

    Prototyped 3d vis

--- tests/3dvis.js
+++ tests/3dvis.js
@@ -6,10 +6,10 @@ init();
 animate();
 
 function init() {
-	container = document.createElement( 'div' );
-	document.body.appendChild( container );
+	container = document.createElement('div');
+	document.body.appendChild(container);
 
-	camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, - 500, 1000 );
+	camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, - 500, 1000);
 	camera.position.x = 200;
 	camera.position.y = 100;
 	camera.position.z = 200;
@@ -18,139 +18,63 @@ function init() {
     group = new THREE.Group();
     scene.add(group);
 /*
-	// Grid
-
-	var size = 500, step = 50;
-
-	var geometry = new THREE.Geometry();
-
-	for ( var i = - size; i <= size; i += step ) {
-
-		geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
-		geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
-
-		geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
-		geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
-
-	}
-
-	var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2 } );
-
-	var line = new THREE.LineSegments( geometry, material );
-	scene.add( line );
-
-	// Cubes
-
-	var geometry = new THREE.BoxGeometry( 50, 50, 50 );
-	var material = new THREE.MeshLambertMaterial( { color: 0xffffff, overdraw: 0.5 } );
-
-	for ( var i = 0; i < 100; i ++ ) {
-
-		var cube = new THREE.Mesh( geometry, material );
-
-		cube.scale.y = Math.floor( Math.random() * 2 + 1 );
-
-		cube.position.x = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
-		cube.position.y = ( cube.scale.y * 50 ) / 2;
-		cube.position.z = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
-
-		scene.add( cube );
-
-	}
-*/
-
-//-180, 0, 0, 0, 0, 0, 1
     var triangleShape = new THREE.Shape();
-    triangleShape.moveTo(  80, 20 );
-    triangleShape.lineTo(  40, 80 );
-    triangleShape.lineTo( 120, 80 );
-    triangleShape.lineTo(  80, 20 ); // close path
-
-    function drawFill(shape, color, x, y, z) {
-        var geometry = new THREE.ShapeGeometry(shape);
-    	var material = new THREE.MeshBasicMaterial( { color: color, overdraw: 0.5, side: THREE.DoubleSide } );
-    	var mesh = new THREE.Mesh( geometry, material );
-    	mesh.position.set(x, y, z);
-    	group.add(mesh);
-
-        console.log('added fill');
-    }
-
-    function drawOutline(shape, color, x, y, z) {
-        var geometry = shape.createPointsGeometry();
-    	geometry.vertices.push( geometry.vertices[ 0 ].clone() );
-    	var material = new THREE.LineBasicMaterial( { linewidth: 10, color: color, transparent: true } );
-    	var line = new THREE.Line( geometry, material );
-    	line.position.set(x, y, z);
-    	group.add( line );
-
-        console.log('added outline');
-    }
-
-    function random(min, max) {
-        // generate random integer
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    function generateColor() {
-        // generate color
-        var color = new THREE.Color('rgb(' + random(0, 255) + ', ' + random(0, 255) + ', ' + random(0, 255) + ')');
-        return color;
-    }
+    triangleShape.moveTo( 80, 20);
+    triangleShape.lineTo( 40, 80);
+    triangleShape.lineTo(120, 80);
 
     drawFill(triangleShape, generateColor(), 0, 0, 0);
     drawOutline(triangleShape, generateColor(), 0, 0, 0);
     drawFill(triangleShape, generateColor(), 100, 0, 0);
-    drawOutline(triangleShape, generateColor(), 100, 0, 0);
-/*
-    var geometry = new THREE.ShapeGeometry(triangleShape);
-	var material = new THREE.MeshBasicMaterial( { color: 0x000000, overdraw: 0.5 } );
-	var mesh = new THREE.Mesh( geometry, material );
-	mesh.position.set(0, 0, 0);
-	mesh.rotation.set(0, 0, 0);
-	mesh.scale.set(1, 1, 1);
-	group.add(mesh);
+    drawOutline(triangleShape, generateColor(), 100, 0, 0);*/
 
-	var geometry = triangleShape.createPointsGeometry();
-	geometry.vertices.push( geometry.vertices[ 0 ].clone() );
-	var material = new THREE.LineBasicMaterial( { linewidth: 10, color: 0x333333, transparent: true } );
-	var line = new THREE.Line( geometry, material );
-	line.position.set(0, 0, 0);
-	line.rotation.set(0, 0, 0);
-	line.scale.set( 1, 1, 1 );
-	group.add( line );*/
+    var points = generatePoints(40);
+    for(var i = 0; i < points.length; i++) {
+        var sorted = getNearestPoints(points[i], points);
+        var geometry = createGeometry(sorted.slice(0, 3));
+
+        var color = generateColor();
+
+        var fillMaterial = new THREE.MeshBasicMaterial({ color: color, transparent: true, opacity: 0.5, side: THREE.DoubleSide });
+        var fillMesh = new THREE.Mesh(geometry, fillMaterial);
+        fillMesh.position.set(0, 0, 0);
+        group.add(fillMesh);
+
+        var frameMaterial = new THREE.MeshBasicMaterial({ color: color, transparent: true, opacity: 0.8, wireframe: true, side: THREE.DoubleSide });
+        var frameMesh = new THREE.Mesh(geometry, frameMaterial);
+        frameMesh.position.set(0, 0, 0);
+        group.add(frameMesh);
+    }
 
 	// Lights
 
-	var ambientLight = new THREE.AmbientLight( Math.random() * 0x10 );
-	scene.add( ambientLight );
+	var ambientLight = new THREE.AmbientLight(Math.random() * 0x10);
+	scene.add(ambientLight);
 
-	var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
+	var directionalLight = new THREE.DirectionalLight(Math.random() * 0xffffff);
 	directionalLight.position.x = Math.random() - 0.5;
 	directionalLight.position.y = Math.random() - 0.5;
 	directionalLight.position.z = Math.random() - 0.5;
 	directionalLight.position.normalize();
-	scene.add( directionalLight );
+	scene.add(directionalLight);
 
-	var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
+	var directionalLight = new THREE.DirectionalLight(Math.random() * 0xffffff);
 	directionalLight.position.x = Math.random() - 0.5;
 	directionalLight.position.y = Math.random() - 0.5;
 	directionalLight.position.z = Math.random() - 0.5;
 	directionalLight.position.normalize();
-	scene.add( directionalLight );
+	scene.add(directionalLight);
 
 	renderer = new THREE.CanvasRenderer();
-	renderer.setClearColor( 0xf0f0f0 );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	container.appendChild( renderer.domElement );
+	renderer.setClearColor(0xf0f0f0);
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	container.appendChild(renderer.domElement);
 
-	window.addEventListener( 'resize', onWindowResize, false );
-
+	window.addEventListener('resize', onWindowResize, false);
 }
 
 function onWindowResize() {
-
 	camera.left = window.innerWidth / - 2;
 	camera.right = window.innerWidth / 2;
 	camera.top = window.innerHeight / 2;
@@ -158,28 +82,87 @@ function onWindowResize() {
 
 	camera.updateProjectionMatrix();
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
-
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-//
-
 function animate() {
-
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
 
 	render();
-
 }
 
 function render() {
-
 	var timer = Date.now() * 0.0001;
 
-	camera.position.x = Math.cos( timer ) * 200;
-	camera.position.z = Math.sin( timer ) * 200;
-	camera.lookAt( scene.position );
+	camera.position.x = Math.cos(timer) * 200;
+	camera.position.z = Math.sin(timer) * 200;
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
+}
 
+// generations
+
+function random(min, max) {
+    // generate random integer
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generatePoints(num) {
+    var points = [];
+
+    for(var i = 0; i < num; i++) {
+        points.push({
+            x: random(-400, 400),
+            y: random(-400, 400),
+            z: random(-400, 400)
+        });
+    }
+
+    return points;
+}
+
+function getNearestPoints(point, points) {
+    var distances = [];
+    var sortedArray = [];
+
+    for(var i = 0; i < points.length; i++) {
+        if(points[i] != point) {
+            distances.push({
+                point: points[i],
+                distance: Math.sqrt(
+                    Math.pow(point.x - points[i].x, 2) +
+                    Math.pow(point.y - points[i].y, 2) +
+                    Math.pow(point.z - points[i].z, 2)
+                )
+            });
+        }
+    }
+
+    distances.sort(function(a, b) {
+        return a.distance - b.distance;
+    });
+
+    for(var i = 0; i < distances.length; i++) {
+        sortedArray.push(distances[i].point);
+    }
+
+    return sortedArray;
+}
+
+function createGeometry(points) {
+    var geometry = new THREE.Geometry();
+
+    for(var i = 0; i < 3; i++) {
+        geometry.vertices.push(new THREE.Vector3(points[i].x, points[i].y, points[i].z));
+    }
+    geometry.faces.push(new THREE.Face3(0, 1, 2));
+    geometry.computeBoundingSphere();
+
+    return geometry;
+}
+
+function generateColor() {
+    var color = new THREE.Color('rgb(' + random(0, 255) + ', ' + random(0, 255) + ', ' + random(0, 255) + ')');
+    return color;
 }
